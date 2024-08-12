93d376a14dcf566424b752a15e84a1995cd002b3
huangqing.zhu <huangqing.zhu@centurygame.com>
2024-08-12 15:56:01 +0800

    refactor: 优化，速度提升

--- reporter.go
+++ reporter.go
@@ -5,7 +5,71 @@ import (
 	"github.com/sandwich-go/boost/z"
 	"github.com/sandwich-go/logbus"
 	"github.com/sandwich-go/logbus/monitor"
+	"sync"
+	"time"
 )
+
+const (
+	renewTimingName  = "siid_renew_time"
+	renewCountName   = "siid_renew"
+	nextTimingName   = "siid_next_time"
+	nextCountName    = "siid_next"
+	quantumGaugeName = "siid_quantum"
+	maxGaugeName     = "siid_max"
+	nleftGaugeName   = "siid_n_left"
+)
+
+var (
+	registerMonitorOnce sync.Once
+
+	renewTimingLatency = newSummaryVec(renewTimingName, "domain", "status")
+	renewCounter       = newCounterVec(renewCountName, "domain", "status")
+	nextTimingLatency  = newSummaryVec(nextTimingName, "domain")
+	nextCounter        = newCounterVec(nextCountName, "domain")
+	quantumGauge       = newGaugeVec(quantumGaugeName, "domain")
+	maxGauge           = newGaugeVec(maxGaugeName, "domain")
+	nleftGauge         = newGaugeVec(nleftGaugeName, "domain")
+)
+
+func newGaugeVec(name string, label ...string) *prometheus.GaugeVec {
+	return prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: name,
+		}, label)
+}
+
+func newSummaryVec(name string, label ...string) *prometheus.SummaryVec {
+	return prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name: name,
+			Objectives: map[float64]float64{
+				0.5:  0.05,
+				0.95: 0.02,
+				0.99: 0.001,
+				1.0:  0,
+			},
+			MaxAge: time.Minute,
+		}, label)
+}
+
+func newCounterVec(name string, label ...string) *prometheus.CounterVec {
+	return prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: name,
+		}, label)
+}
+
+func init() {
+	registerMonitorOnce.Do(func() {
+		monitor.RegisterCollector(renewTimingLatency)
+		monitor.RegisterCollector(renewCounter)
+		monitor.RegisterCollector(nextTimingLatency)
+		monitor.RegisterCollector(nextCounter)
+		monitor.RegisterCollector(quantumGauge)
+		monitor.RegisterCollector(maxGauge)
+		monitor.RegisterCollector(nleftGauge)
+	})
+}
 
 func getRenewStatus(err error) string {
 	if err != nil {
@@ -18,6 +82,7 @@ func (e *engine) renewReport(currQuantum uint64, renewBegin z.MonoTimeDuration, 
 	if !e.builder.visitor.GetEnableMonitor() {
 		return
 	}
+
 	if err != nil {
 		_ = e.renewErrCount.Add(1)
 		logbus.Error(w("renew error"), logbus.ErrorField(err), logbus.String("domain", e.domain))
@@ -29,9 +94,9 @@ func (e *engine) renewReport(currQuantum uint64, renewBegin z.MonoTimeDuration, 
 		}
 	}
 	if e.builder.visitor.GetEnableTimeSummary() {
-		_ = monitor.Timing("siid_renew_time", z.MonoSince(renewBegin), prometheus.Labels{"domain": e.domain, "status": getRenewStatus(err)})
+		renewTimingLatency.WithLabelValues(e.domain, getRenewStatus(err)).Observe(z.MonoSince(renewBegin).Seconds())
 	} else {
-		_ = monitor.Count("siid_renew", 1, prometheus.Labels{"domain": e.domain, "status": getRenewStatus(err)})
+		renewCounter.WithLabelValues(e.domain, getRenewStatus(err)).Inc()
 	}
 }
 
@@ -39,11 +104,12 @@ func (e *engine) nextReport(n int, nextBegin z.MonoTimeDuration, _ error) {
 	if !e.builder.visitor.GetEnableMonitor() {
 		return
 	}
+
 	cost := z.MonoSince(nextBegin)
 	if e.builder.visitor.GetEnableTimeSummary() {
-		_ = monitor.Timing("siid_next_time", cost, prometheus.Labels{"domain": e.domain})
+		nextTimingLatency.WithLabelValues(e.domain).Observe(cost.Seconds())
 	} else {
-		_ = monitor.Count("siid_next", int64(n), prometheus.Labels{"domain": e.domain})
+		nextCounter.WithLabelValues(e.domain).Inc()
 	}
 	if e.builder.visitor.GetEnableSlow() && cost >= e.builder.visitor.GetSlowQuery() {
 		logbus.Warn(w("next slow query"), logbus.Duration("cost", cost), logbus.String("domain", e.domain), logbus.Int("count", n))
@@ -54,14 +120,15 @@ func (e *engine) useNewQuantumReport() {
 	if !e.builder.visitor.GetEnableMonitor() {
 		return
 	}
-	pl := prometheus.Labels{"domain": e.domain}
-	_ = monitor.Gauge("siid_quantum", float64(e.quantum), pl)
-	_ = monitor.Gauge("siid_max", float64(e.max), pl)
+
+	quantumGauge.WithLabelValues(e.domain).Set(float64(e.quantum))
+	maxGauge.WithLabelValues(e.domain).Set(float64(e.max))
 }
 
 func (e *engine) leftReport() {
 	if !e.builder.visitor.GetEnableMonitor() {
 		return
 	}
-	_ = monitor.Gauge("siid_n_left", float64(e.builder.visitor.GetLimitation()-e.n), prometheus.Labels{"domain": e.domain})
+
+	nleftGauge.WithLabelValues(e.domain).Set(float64(e.builder.visitor.GetLimitation() - e.n))
 }
